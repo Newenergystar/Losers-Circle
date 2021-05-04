aa5ef250d6872a097485ee06b389174d01bee62a
Newenergystar <73537729+Newenergystar@users.noreply.github.com>
2021-05-04 20:41:55 +0100

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,18 @@
 <?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
+$cleardb_username = $cleardb_url["befc3846805b02"];
+$cleardb_password = $cleardb_url["8c27a32b"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-$servername = "cs2410-web01pvm.aston.ac.uk";
-$dBUsername = "u-190131063";
-$dBPassword = "aTnarBrzAnALxKP";
-$dBName = " u_190131063_db";
+if (!$conn) {
+	die("Connection failed: ".mysqli_connect_error());
+}
+?>
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
-//if (!$conn) {
-	//die("Connection failed: ".mysqli_connect_error());
-//}
