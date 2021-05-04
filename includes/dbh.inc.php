eaa9b54bad931f05ed86a9cbf30a6d21a638c4c9
Newenergystar <73537729+Newenergystar@users.noreply.github.com>
2021-05-04 20:46:00 +0100

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,11 +4,12 @@ $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
 $cleardb_username = $cleardb_url["befc3846805b02"];
 $cleardb_password = $cleardb_url["8c27a32b"];
-$cleardb_db = substr($cleardb_url["path"],1);
+$cleardb_db = substr($cleardb_url["heroku_fbc7c3b04eb92d5"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 
 if (!$conn) {
 	die("Connection failed: ".mysqli_connect_error());
